18cc1330b642a5ec101002212fdce27774f7d53a
AmirHossein-Mohammadpour <ah.mohammadpour77@gmail.com>
2026-04-28 09:52:53 +0330

    Some Configs about SVG Files and Pathing Done in Config Files

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,11 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   cacheComponents: true,
   allowedDevOrigins: ['192.168.1.101'],
-
+  images: {
+    dangerouslyAllowSVG: true, // Required to use SVGs with next/image
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 };
 
 export default nextConfig;
