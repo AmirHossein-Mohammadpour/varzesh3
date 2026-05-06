e6509b3b41be9b8f688835b1d94d8152bfbcc6d4
AmirHossein-Mohammadpour <ah.mohammadpour77@gmail.com>
2026-05-06 15:40:39 +0330

    navBar is now React to its size-changing by using  ResizeObserver

--- src/_ui/header/navBar.tsx
+++ src/_ui/header/navBar.tsx
@@ -1,7 +1,73 @@
-const NavBar = () => {
+"use client";
+
+import { Menu, MenuIcon } from "lucide-react";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+
+type NavBarProps = {
+  navs: { name: string; url: string }[];
+  logo: StaticImport;
+};
+
+const NavBar = ({ navs, logo }: NavBarProps) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [navWidth, setNavWidth] = useState(0);
+  const nav = useRef<HTMLElement>(null);
+  useEffect(() => {
+    const observer = new ResizeObserver((entries) => {
+      setNavWidth(entries[0].borderBoxSize[0].inlineSize);
+    });
+    if (nav.current) {
+      observer.observe(nav.current);
+    }
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav ref={nav} className={`@container/navBar`}>
+      <div>
+        {navWidth < 768 && (
+          <div className="flex gap-2">
+            <button
+              onClick={() => {
+                setIsMenuOpen(true);
+              }}
+            >
+              <MenuIcon />
+            </button>
+            <Link href={"#"}>
+              <Image src={logo} alt="ورزش ۳" priority />
+            </Link>
+          </div>
+        )}
+        {(isMenuOpen || navWidth >= 768) && (
+          <div onClick={()=>{setIsMenuOpen(false)}} className={`fixed inset-0 @3xl/navBar:static bg-black/30`}>
+            <ul
+              onClick={(e)=>{e.stopPropagation()}}
+              className={``}
+            >
+              <li>
+                <Link href={"#"}>
+                  <Image src={logo} alt="ورزش ۳" priority />
+                </Link>
+              </li>
+              {navs.map((nav, index, navs) => {
+                return (
+                  <li key={index}>
+                    <Link href={nav.url}>{nav.name}</Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
