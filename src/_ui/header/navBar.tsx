d00d6ca32a45b24d2d0117b127b49672da81c2f3
AmirHossein-Mohammadpour <ah.mohammadpour77@gmail.com>
2026-05-09 10:56:38 +0330

    Conditional Rendering of navBar Almost Done.

--- src/_ui/header/navBar.tsx
+++ src/_ui/header/navBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Menu, MenuIcon } from "lucide-react";
+import { Menu } from "lucide-react";
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,10 +8,11 @@ import { useEffect, useRef, useState } from "react";
 
 type NavBarProps = {
   navs: { name: string; url: string }[];
+  anotherNav?: { name: string; url: string }[];
   logo: StaticImport;
 };
 
-const NavBar = ({ navs, logo }: NavBarProps) => {
+const NavBar = ({ navs, logo, anotherNav }: NavBarProps) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [navWidth, setNavWidth] = useState(0);
   const nav = useRef<HTMLElement>(null);
@@ -26,46 +27,65 @@ const NavBar = ({ navs, logo }: NavBarProps) => {
       observer.disconnect();
     };
   }, []);
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+      return () => {
+        document.body.style.overflow = 'unset';
+      };
+    }
+  }, [isMenuOpen]);
 
   return (
-    <nav ref={nav} className={`@container/navBar`}>
-      <div>
-        {navWidth < 768 && (
-          <div className={``}>
-            <button
-              onClick={() => {
-                setIsMenuOpen(true);
-              }}
-            >
-              <MenuIcon />
+    <nav ref={nav} className={`@container/navBar px-2 py-3 bg-black light:bg-white`}>
+        {navWidth < 992 && (
+          <div className={`flex gap-2`}>
+            <button onClick={() => {setIsMenuOpen(true)}}>
+              <Menu className={`light:text-neutral-600`}/>
             </button>
             <Link href={"#"}>
               <Image src={logo} alt="ورزش ۳" priority />
             </Link>
           </div>
         )}
-        {(isMenuOpen || navWidth >= 768) && (
-          <div onClick={()=>{setIsMenuOpen(false)}} className={``}>
+        {(isMenuOpen || navWidth >= 992) && (
+          <div onClick={()=>{setIsMenuOpen(false)}} className={`${(navWidth < 992) ? "fixed inset-0 bg-white/50 light:bg-black/50" : ""}`}>
             <ul
               onClick={(e)=>{e.stopPropagation()}}
-              className={``}
+              className={
+                `flex ${(navWidth < 992) ? "relative w-4/5 max-w-80 h-full p-4 overflow-auto flex-col" : "flex-row items-center"}
+                 text-sm font-medium bg-black light:bg-white text-[#dedede] light:text-[#212121]`
+              }
             >
-              <li>
+              <h2 className={`${(navWidth < 992) ? "":"px-2"}`}>
                 <Link href={"#"}>
                   <Image src={logo} alt="ورزش ۳" priority />
                 </Link>
-              </li>
-              {navs.map((nav, index, navs) => {
+              </h2>
+              <hr className={`mt-4 mb-2 text-neutral-800 light:text-neutral-300`}/>
+              {navs.map((nav, index) => {
                 return (
-                  <li key={index}>
+                  <li key={index} className={`${(navWidth < 992) ? "py-2" : "px-2"} flex flex-col items-stretch ${(["جدول لیگ برتر", "جام جهانی"].includes(nav.name)) && "text-red-600"}`}>
                     <Link href={nav.url}>{nav.name}</Link>
                   </li>
                 );
               })}
+              {
+                <>
+                  <h2 className={`${(navWidth < 992) ? "py-2":"px-2"}`}>سرویس‌ها</h2>
+                  <hr className={`mt-4 mb-2 text-neutral-800 light:text-neutral-300`}/>
+                  {anotherNav?.map((nav, index) => {
+                    return(
+                      <li key={index} className={`${(navWidth < 992) ? "py-2" : "px-2"} flex flex-col items-stretch`}>
+                        <Link href={nav.url}>{nav.name}</Link>
+                      </li>
+                    );
+                  })}
+                </>
+              }
             </ul>
           </div>
         )}
-      </div>
     </nav>
   );
 };
