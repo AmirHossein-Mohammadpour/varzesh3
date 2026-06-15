e47ce20808e79bb970e958114ef2dc1ea3d9e3fd
AmirHossein-Mohammadpour <ah.mohammadpour77@gmail.com>
2026-06-15 11:43:55 +0330

    Slider Component Created.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Box from "@/_ui/rootPage/box";
+import Slider from "@/_ui/rootPage/slider";
 
 const RootPage = () => {
   return (
     <main className={`@container/rootPage`}>
       <div className={`p-2 flex flex-col @lg/rootPage:grid @lg/rootPage:grid-cols-2 @5xl/rootPage:grid-cols-3 @7xl/rootPage:grid-cols-4 gap-3`}>
+        <Slider/>
         <Box title="جام جهانی ۲۰۲۶ آمریکا" className={``}>سلام چطوری؟</Box>
       </div>
     </main>
