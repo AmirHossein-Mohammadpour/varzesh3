63e31dbd82ee7e6e70ad6a408fb011a504722fa1
AmirHossein-Mohammadpour <ah.mohammadpour77@gmail.com>
2026-06-26 10:23:36 +0330

    Swiper Library Added to Project and a Simple Slider Created.

--- src/_ui/rootPage/slider.tsx
+++ src/_ui/rootPage/slider.tsx
@@ -1,6 +1,26 @@
+"use client"
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import {Navigation, Pagination} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <div className={`h-50`}>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation={true}
+        pagination={{clickable: true}}
+        className={`h-full border`}
+      >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
+    </div>
   )
 }
 
