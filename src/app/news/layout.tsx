26e710aa7c0b9f98a15a260f909c23c53d70712d
AmirHossein Mohammadpour <ah.mohammadpour77@gmail.com>
2026-08-17 11:52:19 +0330

    news layout is done

--- src/app/news/layout.tsx
+++ src/app/news/layout.tsx
@@ -1,14 +1,12 @@
 import news from "@/_lib/news";
 import NewsBar from "@/_ui/rootPage/newsBar";
 
-const NewsLayout = ({ children }: LayoutProps<"/news/[newsID]">) => {
+const NewsLayout = ({ children }: LayoutProps<"/news">) => {
   return (
     <div className={`@container/newsLayout`}>
-      <div
-        className={`flex flex-col @3xl/newsLayout:flex-row justify-between gap-5`}
-      >
+      <div className={`flex flex-col @3xl/newsLayout:flex-row justify-between gap-5`}>
         <div className={`flex-2`}>{children}</div>
-        <div className={`flex-1 bg-black light:bg-white text-xs self-center @3xl/newsLayout:self-auto rounded m-2`}>
+        <div className={`flex-1 bg-black light:bg-white text-xs self-center @3xl/newsLayout:self-start rounded m-2`}>
           <NewsBar news={news} />
         </div>
       </div>
